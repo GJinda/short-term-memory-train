3f132e01d8095d0d23c2b94bf2d759b2a06e76db
takaidada <takaidada@163.com>
2024-12-30 18:03:26 +0800

    add input box and check number function

--- short-term-momery.js
+++ short-term-momery.js
@@ -1,9 +1,27 @@
 const $ = (selector) => {
   return document.querySelector(selector)
 }
-
-const selectAll = (selector) =>{
+const selectAll = (selector) => {
   return document.querySelectorAll(selector)
+}
+function addClass(element, className) {
+  element.classList.add(className);
+}
+function removeClass(element, className) {
+  element.classList.remove(className);
+}
+function hasClass(element, className) {
+  return element.classList.contains(className);
+}
+function getChildIndex(child) {
+  var parent = child.parentNode;
+  for (var i = 0; i < parent.children.length; i++) {
+    if (parent.children[i] === child) {
+      return i;
+    }
+  }
+  // cannot find child
+  return -1;
 }
 
 function getRandomArbitrary(min, max) {
@@ -29,30 +47,43 @@ function createDom(tpl) {
   div.innerHTML = tpl;
   return div.children[0];
 }
-function getNumStr(num) {
-  return `
-    <div class='num-box'>${num}</div>
-  `
+function getNumDomStr(num) {
+  return `<div class='num-box'>${num}</div>`
 }
-function appendNum(numStr) {
-  const NumDom = createDom(numStr);
-  console.log('NumDom')
-  console.log(NumDom)
-  numContainer.appendChild(NumDom);
+function getInputDomStr() {
+  return `<input class="input-box" type="text">`
+}
+function appendNum(numDomStr) {
+  const numDom = createDom(numDomStr);
+  numContainer.appendChild(numDom);
+}
+function appendInput(inputDomStr) {
+  const inputDom = createDom(inputDomStr);
+  inputDom.onblur = (event) => {
+    let inputValue = event.target.value
+    if (inputValue) {
+      console.log(inputValue)
+      console.log(shuffleArray[index])
+      let index = getChildIndex(event.target)
+      if (inputValue == shuffleArray[index]) {
+        addClass(event.target, 'hide')
+      }
+    }
+  }
+  inputContainer.appendChild(inputDom);
 }
 function createNumDom() {
   shuffleArray.forEach(element => {
-    let numStr = getNumStr(element)
-    appendNum(numStr)
+    let numDomStr = getNumDomStr(element)
+    let inputDomStr = getInputDomStr()
+    appendNum(numDomStr)
+    appendInput(inputDomStr)
   });
 }
 
 //get dom
 const numContainer = $('.num-container')
-const inputs = selectAll('ipnut')
-console.log('inputs')
-console.log(inputs)
-
+const inputContainer = $('.input-container')
 
 //get random number array
 const numArray = getNumArray()
@@ -61,4 +92,3 @@ const shuffleArray = getShuffleArray(numArray)
 console.log('shuffleArray')
 console.log(shuffleArray)
 createNumDom()
-
